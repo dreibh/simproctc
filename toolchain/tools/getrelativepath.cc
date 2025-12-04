1351a27a254699c65093ed7cd45e71e33fe69b51
Thomas Dreibholz <dreibh@simula.no>
2025-12-04 15:38:15 +0100

    Header updates.

--- toolchain/tools/getrelativepath.cc
+++ toolchain/tools/getrelativepath.cc
@@ -1,6 +1,6 @@
 // ###########################################################################
 //             Thomas Dreibholz's R Simulation Scripts Collection
-//                  Copyright (C) 2004-2025 Thomas Dreibholz
+//                  Copyright (C) 2004-2026 Thomas Dreibholz
 //
 //           Author: Thomas Dreibholz, thomas.dreibholz@gmail.com
 // ###########################################################################
